6b8768cb17e7e50b5f572497c4384a612c78858d
Alex Andrei <alexandruene37@gmail.com>
2024-04-09 12:47:25 +0300

    B full

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/Constants.java
@@ -0,0 +1,11 @@
+package ro.pub.cs.systems.eim.practicaltest01var04;
+
+public class Constants {
+
+    public static final String STUDENT_NAME = "edit_box_student_name";
+    public static final String CLASS_NUMBER = "edit_box_class_number";
+
+    public static final String STUDENT_NAME_CHECKBOX = "check_box_student_name";
+
+    public static final String CLASS_NUMBER_CHECKBOX = "check_box_class_number";
+}
--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PracticalTest01Var04MainActivity extends AppCompatActivity {
@@ -63,5 +64,29 @@ public class PracticalTest01Var04MainActivity extends AppCompatActivity {
 
     private void showToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(Constants.STUDENT_NAME, studentNameEditText.getText().toString());
+        outState.putString(Constants.CLASS_NUMBER, classNumberEditText.getText().toString());
+        outState.putBoolean(Constants.STUDENT_NAME_CHECKBOX, studentNameCheckBox.isChecked());
+        outState.putBoolean(Constants.CLASS_NUMBER_CHECKBOX, classNumberCheckBox.isChecked());
+    }
+
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState.containsKey(Constants.STUDENT_NAME)) {
+            studentNameEditText.setText(savedInstanceState.getString(Constants.STUDENT_NAME));
+        }
+        if (savedInstanceState.containsKey(Constants.CLASS_NUMBER)) {
+            classNumberEditText.setText(savedInstanceState.getString(Constants.CLASS_NUMBER));
+        }
+        if (savedInstanceState.containsKey(Constants.STUDENT_NAME_CHECKBOX)) {
+            studentNameCheckBox.setChecked(savedInstanceState.getBoolean(Constants.STUDENT_NAME_CHECKBOX));
+        }
+        if (savedInstanceState.containsKey(Constants.CLASS_NUMBER_CHECKBOX)) {
+            classNumberCheckBox.setChecked(savedInstanceState.getBoolean(Constants.CLASS_NUMBER_CHECKBOX));
+        }
     }
 }
